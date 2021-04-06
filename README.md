50b679d7ddab3f704a03668f46a0844bee60d0be
Sridhar Ratnakumar <srid@srid.ca>
2021-04-06 18:24:07 -0400

    default.nix not required

---
+++ README.md
@@ -0,0 +1,9 @@
+Requires Nix Flakes.
+
+```
+# dev shell
+nix develop
+
+# or just run directly
+nix develop -c cargo run
+```
--- default.nix
+++
@@ -1,9 +0,0 @@
-let 
-    pkgs = import ./dep/nixpkgs { overlays = [ (import ./dep/rust-overlay) ]; };
-    rust = pkgs.rust-bin.stable.latest.rust;
-in 
-    pkgs.mkShell {
-        buildInputs = [
-            rust
-        ];
-    }
