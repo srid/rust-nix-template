919e00831eaf555eaa65f14fcc60d3e740120504
Sridhar Ratnakumar <srid@srid.ca>
2021-04-07 18:59:51 -0400

    Switch to nmattia/naersk (but without overlay)

--- flake.nix
+++ flake.nix
@@ -4,22 +4,44 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, flake-compat }:
-    flake-utils.lib.eachDefaultSystem
+  outputs = { self, nixpkgs, utils, rust-overlay, naersk, ... }:
+    utils.lib.eachDefaultSystem
       (system:
-        let pkgs = import nixpkgs {
+        /* let pkgs = import nixpkgs {
           inherit system;
           overlays = [ rust-overlay.overlay ];
-        }; in
-        {
+        }; */
+        let 
+          pkgs = nixpkgs.legacyPackages."${system}";
+          naersk-lib = naersk.lib."${system}";
+        in rec {
+          # `nix build`
+          packages.bouncy = naersk-lib.buildPackage {
+            pname = "bouncy";
+            root = ./.;
+          };
+          defaultPackage = packages.bouncy;
+
+          # `nix run`
+          apps.bouncy = utils.lib.mkApp {
+            drv = packages.bouncy;
+          };
+          defaultApp = apps.bouncy;
+
+          # `nix develop`
           devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+          /* devShell = pkgs.mkShell {
             nativeBuildInputs = [
               (pkgs.rust-bin.stable.latest.default.override {
                 extensions = [
@@ -33,7 +55,7 @@
                 ];
               })
             ];
-          };
+          }; */
         }
       );
 }
