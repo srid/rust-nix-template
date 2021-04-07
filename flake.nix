7e6a9d6b1d06506cdc575e395b8594a20cbc0b51
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2021-04-07 19:07:41 -0400

    naersk (#2)
    * Switch to nmattia/naersk (but without overlay)
    
    * Restore the overlay

--- flake.nix
+++ flake.nix
@@ -4,35 +4,50 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, flake-compat }:
-    flake-utils.lib.eachDefaultSystem
+  outputs = { self, nixpkgs, utils, rust-overlay, naersk, ... }:
+    utils.lib.eachDefaultSystem
       (system:
-        let pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlay ];
-        }; in
-        {
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = [
-              (pkgs.rust-bin.stable.latest.default.override {
-                extensions = [
-                  "rust-src"
-                  "cargo"
-                  "rustc"
-                  "rls"
-                  "rust-analysis"
-                  "rustfmt"
-                  "clippy"
-                ];
+        let 
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ 
+              rust-overlay.overlay
+              (self: super: {
+                # Because rust-overlay bundles multple rust packages into one
+                # derivation, specify that mega-bundle here, so that naersk
+                # will use them automatically.
+                rustc = self.rust-bin.stable.latest.default;
+                cargo = self.rust-bin.stable.latest.default;
               })
             ];
+          };
+          naersk-lib = naersk.lib."${system}";
+        in rec {
+          # `nix build`
+          packages.bouncy = naersk-lib.buildPackage {
+            pname = "bouncy";
+            root = ./.;
+          };
+          defaultPackage = packages.bouncy;
+
+          # `nix run`
+          apps.bouncy = utils.lib.mkApp {
+            drv = packages.bouncy;
+          };
+          defaultApp = apps.bouncy;
+
+          # `nix develop`
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
           };
         }
       );
