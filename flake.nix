92d9de855ad08bac02cc3fe52243c4189f86c2c3
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2021-04-07 22:39:02 -0400

    Try crate2nix (#3)
    * WIP
    
    * Fix and add gitignore
    
    * Finish it

--- flake.nix
+++ flake.nix
@@ -3,43 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
+    gitignore = { 
+      url = "github:hercules-ci/gitignore"; 
+      flake=false; 
+    };
+    crate2nix = {
+      url = "github:balsoft/crate2nix/tools-nix-version-comparison";
+      flake = false;
+    };
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, naersk, ... }:
+  outputs = { self, nixpkgs, utils, gitignore, crate2nix, ... }:
     utils.lib.eachDefaultSystem
       (system:
         let 
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ 
-              rust-overlay.overlay
-              (self: super: {
-                # Because rust-overlay bundles multple rust packages into one
-                # derivation, specify that mega-bundle here, so that naersk
-                # will use them automatically.
-                rustc = self.rust-bin.stable.latest.default;
-                cargo = self.rust-bin.stable.latest.default;
-              })
-            ];
+          pkgs = import nixpkgs { inherit system; };
+          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
+            generatedCargoNix;
+          inherit (import gitignore { inherit (pkgs) lib; }) gitignoreSource;
+          project = pkgs.callPackage (generatedCargoNix {
+            name = "bouncy";
+            src = gitignoreSource ./.;
+          }) {
+            # Individual crate overrides go here
+            # Example: https://github.com/balsoft/simple-osd-daemons/blob/6f85144934c0c1382c7a4d3a2bbb80106776e270/flake.nix#L28-L50
           };
-          naersk-lib = naersk.lib."${system}";
+          packages = builtins.mapAttrs (name: member: member.build) project.workspaceMembers;
         in rec {
+          inherit packages;
+
           # `nix build`
-          packages.bouncy = naersk-lib.buildPackage {
-            pname = "bouncy";
-            root = ./.;
-          };
           defaultPackage = packages.bouncy;
 
           # `nix run`
           apps.bouncy = utils.lib.mkApp {
+            name = "bouncy";
             drv = packages.bouncy;
           };
           defaultApp = apps.bouncy;
